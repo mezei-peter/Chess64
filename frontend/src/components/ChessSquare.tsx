411ad61d23012f8501c2857160504b723641f2d5
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-31 18:27:36 +0200

    Add FreeSerif and remake chess pieces

--- frontend/src/components/ChessSquare.tsx
+++ frontend/src/components/ChessSquare.tsx
@@ -3,7 +3,7 @@ function ChessSquare({piece, dark}: { piece: string, dark: boolean }) {
         <div
             className={
                 dark
-                    ? `h-[4.3vmax] w-[4.3vmax] flex justify-center items-center bg-gray-500`
+                    ? `h-[4.3vmax] w-[4.3vmax] flex justify-center items-center bg-gray-200`
                     : `h-[4.3vmax] w-[4.3vmax] flex justify-center items-center bg-gray-100`
             }>
             {piece !== '0' && piece}
--- frontend/src/components/PlayArea.tsx
+++ frontend/src/components/PlayArea.tsx
@@ -18,7 +18,7 @@ function PlayArea({handleQuit}: { handleQuit: () => void }) {
     const [board, _setBoard] = useState(initBoard());
 
     return (
-        <div className={"border border-red-400 h-5/6 flex flex-col justify-between items-center"}>
+        <div className={"h-5/6 flex flex-col justify-between items-center"}>
             <ChessBoard board={board}/>
             <button onClick={handleQuit} className="bg-red-800 hover:bg-red-900 text-white font-semibold py-2 px-4
                 rounded shadow">
