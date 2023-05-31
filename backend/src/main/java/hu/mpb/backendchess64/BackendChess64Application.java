9363597631681d200f8da8f52599c8a8ce72a9a4
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-31 11:29:13 +0200

    Initialize project

---
+++ backend/src/main/java/hu/mpb/backendchess64/BackendChess64Application.java
@@ -0,0 +1,13 @@
+package hu.mpb.backendchess64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChess64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChess64Application.class, args);
+	}
+
+}
