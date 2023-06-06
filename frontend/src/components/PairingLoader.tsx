d90e86a50f258390f8b6005cb160211785b4f864
mezeipb <mezei.peter.barnabas@gmail.com>
2023-06-06 16:28:48 +0200

    Connect to BE with SockJS and StompJS

--- frontend/src/components/PairingLoader.tsx
+++ frontend/src/components/PairingLoader.tsx
@@ -1,6 +1,16 @@
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+import sockConstants from "../constants/sockConstants";
+import {useEffect, useRef} from "react";
 
 function PairingLoader() {
+    const sock = useRef(new SockJS(`${sockConstants.root}/${sockConstants.chess64}`));
+    const client = useRef(Stomp.over(sock.current));
 
+    useEffect(() => {
+        client.current.connect({}, () => console.log("Connected to Chess64"),
+            () => console.error("Connection to Chess64 failed"));
+    }, []);
 
     return (
         <div className={"animate-pulse m-auto text-gray-800 text-2xl p-2"}>
---
+++ frontend/src/constants/sockConstants.ts
@@ -0,0 +1,4 @@
+export default {
+    root: "http://localhost:8080",
+    chess64: "chess64-ws",
+};
