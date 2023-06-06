b0ce381eb4fed9eabd455d1f77f21eb803fb9b03
mezeipb <mezei.peter.barnabas@gmail.com>
2023-06-06 18:06:32 +0200

    Limit name length on FE

--- frontend/src/components/PlayForm.tsx
+++ frontend/src/components/PlayForm.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+const maxNameLength = 15;
+
 function PlayForm({playerName, setPlayerName, handleSubmit}: {
     playerName: string, setPlayerName: React.Dispatch<React.SetStateAction<string>>,
     handleSubmit: () => void
@@ -12,9 +14,13 @@ function PlayForm({playerName, setPlayerName, handleSubmit}: {
               }}>
             <input
                 className="shadow appearance-none border rounded py-2 px-3 text-gray-700
-                leading-tight focus:outline-none focus:shadow-outline w-2/4 relative bottom-4"
+                leading-tight focus:outline-none focus:shadow-outline w-64 relative bottom-4 text-center"
                 id="username" type="text" placeholder="Name" value={playerName}
-                onChange={event => setPlayerName(event.target.value)}/>
+                onChange={event => {
+                    if (event.target.value.length <= maxNameLength) {
+                        setPlayerName(event.target.value)
+                    }
+                }}/>
             <button
                 className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4
                 border border-gray-400 rounded shadow">
