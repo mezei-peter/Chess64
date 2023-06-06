4ca14fadeb88a06892a5bd63aacf4081a2018690
mezeipb <mezei.peter.barnabas@gmail.com>
2023-06-06 15:32:53 +0200

    Create PairingLoader component on FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import PlayArea from "./components/PlayArea";
 import PlayForm from "./components/PlayForm";
 import {useState} from "react";
-
+import PairingLoader from "./components/PairingLoader";
 
 
 function App() {
+    const [freshStart, setFreshStart] = useState(true);
+    const [waiting, setWaiting] = useState(false);
     const [playing, setPlaying] = useState(false);
+    const [playerId, setPlayerId] = useState("");
 
     function startPlaying() {
         setPlaying(true);
+    }
+
+    function startPairing() {
+        setFreshStart(false);
+        setWaiting(true);
     }
 
     function quitPlaying() {
@@ -18,7 +26,9 @@ function App() {
     return (
         <div className={"flex flex-col justify-between h-screen w-screen"}>
             <header className={"text-center text-5xl text-gray-800"}>Chess64</header>
-            {!playing ? <PlayForm handleSubmit={startPlaying}/> : <PlayArea handleQuit={quitPlaying}/>}
+            {freshStart && <PlayForm handleSubmit={startPairing}/>}
+            {waiting && <PairingLoader/>}
+            {playing && <PlayArea handleQuit={quitPlaying}/>}
             <footer className={""}>🄯 2023 MPB. All rights reversed.</footer>
         </div>
     )
---
+++ frontend/src/components/PairingLoader.tsx
@@ -0,0 +1,10 @@
+
+function PairingLoader() {
+    return (
+        <div className={"animate-pulse m-auto text-gray-800 text-2xl p-2"}>
+            Looking for an opponent . . .
+        </div>
+    );
+}
+
+export default PairingLoader;
