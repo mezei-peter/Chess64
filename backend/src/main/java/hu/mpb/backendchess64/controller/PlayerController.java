4f2124aa137fcb01c544e79e573428631ea0e35d
mezeipb <mezei.peter.barnabas@gmail.com>
2023-06-06 13:48:16 +0200

    Remove unused import

--- backend/src/main/java/hu/mpb/backendchess64/controller/PlayerController.java
+++ backend/src/main/java/hu/mpb/backendchess64/controller/PlayerController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/player")
 public class PlayerController {
